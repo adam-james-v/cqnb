2b2812603b3f87ddf1a3c101261d7734a70a71af
Adam (Rusty) Vermeer <RustyVermeer@users.noreply.github.com>
2017-10-11 08:42:56 -0400

    Improve cqjupyter.py
    changed:
    - build_object is now show_object, as per change in CQGI
    - size of 3d display window is increased to take advantage of full width of notebook widget area
    Added:
    - color, scale, units interactions for every show_object
    - export button created after running display interaction
    - export: STEP, JSON, STL, SVG

--- cqjupyter.py
+++ cqjupyter.py
@@ -1,39 +1,56 @@
+"""
+    Jupyter Extension for cadquery script evaluation and display
+
+    The goal of this extension is to allow cadquery to be used easily and
+    reliably within Jupyter Notebooks. It aims to keep feature parity with
+    the Cadquery FreeCAD implementation, and seeks to be one of the main
+    alternative GUIs that cadquery users can rely on for day-to-day CQ use.abs
+
+    For further resources on cadquery, see https://github.com/dcowden/cadquery
+"""
+
+
+import sys
+import os
+import StringIO
+import json
 import cadquery as cq
 from cadquery import cqgi, exporters
 from IPython.display import display
 import pythreejs
 import numpy as np
 import matplotlib
-import sys, os, StringIO, json
+import matplotlib.colors as colors
 import ipywidgets
 
 # TODO: rename and re-organize functions
 # TODO: save into cqnb folder
-# TODO: figure out how to propery do install (adding cqnb_start.py to the startup folder seems incorrect)
+# TODO: figure out how to properly do Extension installation
 
 # The following bits are just to block print output while CQGI parses script
-# Without using these, it might print 'unable to handle function' multiple times, which clutters the output space
+# Make block/enable print  functions to hide excessive stdout writes from CQGI
 # save ref to IPython stdout so that it can be restored if switched away from
-nb_stdout = sys.stdout
-
+NB_STDOUT = sys.stdout
 # Disable
-def blockPrint():
+def block_print():
+    'block stdout'
     sys.stdout = open(os.devnull, 'w')
 
 # Restore
-def enablePrint():
-    sys.stdout = nb_stdout
+def enable_print():
+    'enable stdout'
+    sys.stdout = NB_STDOUT
 
 
 # This Module expects a cadquery object
 
-def cqdisplay(result, color='#708090', cam_dist=50, fov=35, display_id=None):
-
+def cqdisplay(result, color='#708090', scale=1.0):
+    'display CQ object in a ThreeJS Webgl context'
     # Open stream
     output = StringIO.StringIO()
 
     # cadquery will stream a ThreeJS JSON (using old v3 schema, which is deprecated)
-    exporters.exportShape(result, 'TJS', output)
+    exporters.exportShape(result.shape.findSolid().scale(scale), 'TJS', output)
 
     # store stream to a variable
     contents = output.getvalue()
@@ -68,29 +85,34 @@ def cqdisplay(result, color='#708090', cam_dist=50, fov=35, display_id=None):
         entry.pop(0)
         faces.append(entry)
 
-    # Cadquery does not supply face normals in the JSON, and we cannot use THREE.JS built in 'computeFaceNormals' -- at least not easily
-    # Instead, we just calculate the face normals ourselves. It is just the cross product of 2 vectors in the triangle.
+    # Cadquery does not supply face normals in the JSON,
+    # and we cannot use THREE.JS built in 'computefaceNormals'
+    # (at least, not easily)
+    # Instead, we just calculate the face normals ourselves.
+    # It is just the cross product of 2 vectors in the triangle.
     # TODO: see if there is a better way to achieve this result
-    faceNormals = []
+    face_normals = []
 
     for entry in faces:
-        vA = np.asarray(vertices[entry[0]])
-        vB = np.asarray(vertices[entry[1]])
-        vC = np.asarray(vertices[entry[2]])
+        v_a = np.asarray(vertices[entry[0]])
+        v_b = np.asarray(vertices[entry[1]])
+        v_c = np.asarray(vertices[entry[2]])
 
-        vecA = vB - vA
-        vecB = vC - vA
+        vec_a = v_b - v_a
+        vec_b = v_c - v_a
 
-        cross = np.cross(vecA, vecB)
+        cross = np.cross(vec_a, vec_b)
 
-        faceNormals.append([cross[0], cross[1], cross[2]])
+        face_normals.append([cross[0], cross[1], cross[2]])
 
     # set up geometry
-    geom = pythreejs.PlainGeometry(vertices=vertices, faces=faces, faceNormals=faceNormals)
-    mtl = pythreejs.LambertMaterial(color = color, shading = 'FlatShading')
-    obj = pythreejs.Mesh(geometry=geom, material = mtl)
+    geom = pythreejs.PlainGeometry(vertices=vertices, faces=faces, faceNormals=face_normals)
+    mtl = pythreejs.LambertMaterial(color=color, shading='FlatShading')
+    obj = pythreejs.Mesh(geometry=geom, material=mtl)
 
     # set up scene and camera
+    cam_dist = 50
+    fov = 35
     cam = pythreejs.PerspectiveCamera(
         position=[cam_dist, cam_dist, cam_dist], fov=fov,
         children=[pythreejs.DirectionalLight(color='#ffffff', position=[-3, 5, 1], intensity=0.45)])
@@ -100,30 +122,84 @@ def cqdisplay(result, color='#708090', cam_dist=50, fov=35, display_id=None):
     ]
     scn = pythreejs.Scene(children=scn_chld)
 
-    render = pythreejs.Renderer(camera=cam, scene = scn, controls=[pythreejs.OrbitControls(controlling=cam)])
+    render = pythreejs.Renderer(
+        width='830'.decode('utf-8'),
+        height='553'.decode('utf-8'),
+        camera=cam,
+        scene=scn,
+        controls=[pythreejs.OrbitControls(controlling=cam)]
+        )
 
     return render
 
+def cqgen(result, name='Output', color='#708090'):
+    'generate a .JSON file for ThreeJS objects.'
+    # Open stream
+    output = StringIO.StringIO()
 
-def build_object(result):
+    # cadquery will stream a ThreeJS JSON (using old v3 schema, which is deprecated)
+    exporters.exportShape(result, 'TJS', output)
+
+    # store stream to a variable
+    contents = output.getvalue()
+
+    # Close stream
+    output.close()
+
+    # Overwrite the JSON color portion with user color. Disallows NAMED colors
+    col = list(colors.hex2color(color))
+    old_col_str = '"colorDiffuse" : [0.6400000190734865, 0.10179081114814892, 0.126246120426746]'
+    new_col_str = '"colorDiffuse" : ' + str(col)
+    new_contents = contents.replace(old_col_str, new_col_str)
+
+
+    file_name = name + '.json'
+    # Save the string to a json file
+    with open(file_name, "w") as text_file:
+        text_file.write(new_contents)
+
+    # print "Part generated : " + file_name
+
+    return
+
+
+def show_object(result):
+    'returns an object to the Jupyter Widgets area with interactive options'
     # Get the Script's text from the IPython shell's history
-    # TODO: sanitizing function that removes dangerous imports. Eg. strip any lines that have 'import cqjupyter'
-    # luckily, the cell which has just been run by user is immediately placed as the latest entry in the history
-    scriptText = get_ipython().history_manager.input_hist_raw[-1]
+    # TODO: sanitizing function that removes dangerous imports.
+    # Eg. strip any lines that have 'import cqjupyter'
+    # luckily, the cell which has just been run by user
+    # is immediately placed as the latest entry in the history
+    script_text = get_ipython().history_manager.input_hist_raw[-1]
+
+    def find_between(src, first, last):
+        'returns string between two specified substrings'
+        # credit: https://stackoverflow.com/a/3368991
+        try:
+            start = src.index(first) + len(first)
+            end = src.index(last, start)
+            return src[start:end]
+        except valueError:
+            return ""
+
+    obj_name = find_between(script_text, 'show_object(', ')')
 
     # A representation of the CQ script with all the metadata attached
-    # stop parse func from printing to the shell. Do this to avoid excessive 'unable to handle function call' warnings
+    # stop parse func from printing to the shell to avoid excessive warnings
     # TODO: fix this the real way... find out proper way to handle this from CQGI author
-    blockPrint()
-    cqModel = cqgi.parse(scriptText)
+    block_print()
+    # change show_object() to build_object until I have opportunity to update CQ...
+    # script_text = script_text.replace('show_object(', 'build_object(')
+    cq_model = cqgi.parse(script_text)
     # re-enable printing to shell
-    enablePrint()
+    enable_print()
 
-    build_result = cqModel.build()
+    build_result = cq_model.build()
 
     # function that creates and updates the model view along with param. interactions
     # kwargs is built up in a loop over the parameters which CQGI supplies
-    def f(**kwargs):
+    def mkui(**kwargs):
+        'assembles widgets area according to parameters found in parsed script'
         new_vals = {}
         # kwargs is linked to ipywidget interactive vals
         # set the kwarg's name as key, set kwarg's value as value.
@@ -132,49 +208,156 @@ def build_object(result):
         for arg in kwargs:
             new_vals[arg] = kwargs[arg]
         try:
-            f.base.close()
-        except:
+            mkui.base.close()
+            mkui.export_filename.close()
+            mkui.export_filetype.close()
+            mkui.export_button.close()
+        except AttributeError:
             pass
-        f.base = update_build(cqModel, new_vals)
-        display(f.base)
-    f.base = None
+        color = mkui.display_options['display_color'].value
+        units = mkui.display_options['display_units'].value
+        scale = mkui.display_options['display_scale'].value
+
+        if units == 'in':
+            final_scale = scale*25.4
+        else:
+            final_scale = scale
+
+        mkui.base, mkui.new_model = update_build(cq_model, new_vals, color=color, scale=final_scale)
+
+        # Create the Export options + Button
+        mkui.export_filename = ipywidgets.Text(
+            description='Filename',
+            value=obj_name,
+            continuous_update=False
+            )
+        mkui.export_filetype = ipywidgets.Dropdown(
+            description='Filetype',
+            options=['STEP', 'JSON', 'STL', 'SVG'],
+            value='STEP',
+            continuous_update=False
+            )
+
+        def export_function(button):
+            'executes export of user selected filetype, only on button press'
+            filename = mkui.export_filename.value
+            filetype = mkui.export_filetype.value
+            fullname = filename + '.' + filetype
+            if filetype == 'STEP':
+                mkui.new_model.shape.findSolid().scale(final_scale).exportStep(fullname)
+            if filetype == 'STL':
+                mkui.new_model.shape.findSolid().scale(final_scale).wrapped.exportStl(fullname)
+            elif filetype == 'JSON':
+                cqgen(mkui.new_model.shape.findSolid().scale(final_scale), name=filename)
+            elif filetype == 'SVG':
+                cq.CQ(mkui.new_model.shape.findSolid().scale(final_scale)).exportSvg(fullname)
+            else:
+                print 'nothing exported, sorry'
+            print 'exported model as: ' + fullname
+
+        gui_layout = ipywidgets.Layout(
+            display='flex',
+            justify_content='center',
+            align_items='center',
+            max_width='30%',
+        )
+        options_layout = ipywidgets.Layout(
+            max_width='50%',
+        )
+
+        mkui.export_button = ipywidgets.Button(description='Export', continuous_update=False)
+        mkui.export_button.on_click(export_function)
+        export_gui = ipywidgets.HBox([
+            ipywidgets.VBox([mkui.export_filename, mkui.export_filetype]),
+            mkui.export_button
+            ])
+
+        render_window = ipywidgets.VBox([mkui.base, export_gui])
+        display(render_window)
+
+    mkui.base = None
+    mkui.export_filename = None
+    mkui.export_filetype = None
+    mkui.export_button = None
 
     # Make sure that the build was successful
     if build_result.success:
-        # Allows us to present parameters for editing throuhg some interface
-        params = cqModel.metadata.parameters
-        interactions = cq_interact(params)
-
+        # Allows us to present parameters for editing through some interface
+        params = cq_model.metadata.parameters
+        interactions, mkui.display_options = cq_interact(params)
         # Display all the results that the user requested
-        for result in build_result.results:
-            # Render the solid and its parameter interactions
-            ipywidgets.interact_manual(f, **cq_interact(params));
+        # for result in build_result.results:
+        # Render the solid and its parameter interactions
+        # display(mkui.display_options)
+        display(mkui.display_options['display_color'])
+        display(mkui.display_options['display_units'])
+        display(mkui.display_options['display_scale'])
+        ipywidgets.interact_manual(mkui, **interactions)
         return
     else:
         print "Error executing CQGI-compliant script."
 
 def cq_interact(params):
+    'builds dict of interaction widgets for each exposed parameter'
     interactions = {}
+    display_options = {}
     for key in params:
         name = params[key].name
         val = params[key].default_value
-        if type(val) is int:
-            interactions[name] = ipywidgets.IntText(description=name, value=val, continuous_update=False)
-        elif type(val) is float:
-            interactions[name] = ipywidgets.FloatText(description=name, value=val, continuous_update=False)
-        elif type(val) is bool:
-            interactions[name] = ipywidgets.Checkbox(description=name, value=val, continuous_update=False)
+        if isinstance(val, int):
+            if isinstance(val, bool):
+                interactions[name] = ipywidgets.Checkbox(
+                    description=name,
+                    value=val,
+                    continuous_update=False
+                    )
+            else:
+                interactions[name] = ipywidgets.IntText(
+                    description=name,
+                    value=val,
+                    continuous_update=False
+                    )
+        elif isinstance(val, float):
+            interactions[name] = ipywidgets.FloatText(
+                description=name,
+                value=val,
+                continuous_update=False
+                )
         else:
-            interactions[name] = ipywidgets.Text(description=name, value=val, continuous_update=False)
-    return interactions
+            interactions[name] = ipywidgets.Text(
+                description=name,
+                value=val,
+                continuous_update=False
+                )
 
-def update_build(model, build_parameters, build_options=None):
+    # add color, units, scale selection
+    display_options['display_color'] = ipywidgets.ColorPicker(
+        description='Color: ',
+        value='#8dc63f',
+        concise=False,
+        continuous_update=False
+        )
+    display_options['display_units'] = ipywidgets.ToggleButtons(
+        description='Units: ',
+        options=['mm', 'in'],
+        value='mm',
+        continuous_update=False
+        )
+    display_options['display_scale'] = ipywidgets.FloatText(
+        description='Scale: ',
+        value=1.0,
+        continuous_update=False
+        )
+
+    return interactions, display_options
+
+def update_build(model, build_parameters, build_options=None, color="#708090", scale=1.0):
+    'updates an object given new parameter values from Jupyter widgets'
     build_result = model.build(build_parameters=build_parameters, build_options=build_options)
     if build_result.success:
-        # TODO: fix this for loop. I'm returning in there... so only need to grab 1 entry from the list.
-        # NOTE: I don't know why there is a list or under what circumstances there will be more than 1 entry
+        # TODO: fix this for loop.
         # whatever the case is, this for loop won't handle that scenario properly yet
         for result in build_result.results:
             # Render the solid
-            render = cqdisplay(result)
-            return render
+            render = cqdisplay(result, color=color, scale=scale)
+            return render, result
